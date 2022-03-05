f7ce2dfc6e8f58cc6db2ea3da551c7ab22514f67
honeypotfly <honeypotfly@protonmail.com>
2022-03-05 18:30:40 -0500

    set up a simple, single port scanner so that we have something to work with

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+func scanPort(protocol string, hostname string, port int, maxTimeout time.Duration) bool {
+	var address string = hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, maxTimeout*time.Second)
+
+	if err != nil {
+		return false
+	}
+
+	defer conn.Close()
+	return true
+}
+
 func main() {
-	println("hello")
+
+	// Defining CLI Flags
+	protoPTR := flag.String("protocol", "tcp", "Set the protocol you want, TCP or UDP. DEFAULT: TCP")
+	hostPTR := flag.String("hostname", "localhost", "Set the hostname you want to connect to. DEFAULT: localhost")
+	portPtr := flag.Int("port", 80, "Set the port to scan. DEFAULT: 80")
+	timeoutPtr := flag.Duration("timeout", 60, "Set the timout allowed on ports. DEFAULT: 60")
+
+	// Parse Flags
+	flag.Parse()
+	open := scanPort(*protoPTR, *hostPTR, *portPtr, *timeoutPtr)
+	open2 := scanPort(*protoPTR, *hostPTR, 40, *timeoutPtr)
+
+	fmt.Printf("Connected? %t\n", open)
+	fmt.Printf("Connected? %t\n", open2)
+
 }
