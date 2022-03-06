7c6daaa82e172af105c1cf6ddcca543e136d9f5b
honeypotfly <honeypotfly@protonmail.com>
2022-03-06 00:46:54 -0500

    set a timeout that allows for an actual connection to the port, otherwise we get false negatives

--- main.go
+++ main.go
@@ -4,25 +4,62 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"os/exec"
 	"strconv"
+	"strings"
 	"time"
+
+	"golang.org/x/sync/semaphore"
 )
 
-func scanPort(protocol string, hostname string, port int, maxTimeout time.Duration) bool {
-	var address string = hostname + ":" + strconv.Itoa(port)
-	conn, err := net.DialTimeout(protocol, address, maxTimeout*time.Second)
+type portScanner struct {
+	hostname string
+	lock     *semaphore.Weighted
+}
+
+func Ulimit() int64 {
+	out, err := exec.Command("ulimit", "-n").Output()
 
 	if err != nil {
+		panic(err)
+	}
+
+	var s string = strings.TrimSpace(string(out))
+	i, err := strconv.ParseInt(s, 10, 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+func scanPort(protocol string, hostname string, port int, maxTimeout time.Duration) bool {
+	address := fmt.Sprintf("%s:%d", hostname, port)
+	fmt.Println(address)
+
+	conn, err := net.DialTimeout(protocol, address, maxTimeout*time.Millisecond)
+	fmt.Println(conn)
+
+	if err != nil {
+		if strings.Contains(err.Error(), "too many open files") {
+			time.Sleep(maxTimeout)
+			// If we open too many files, sleep and restart the scan
+			scanPort(protocol, hostname, port, maxTimeout*time.Second)
+		} else {
+			fmt.Println(address, "is closed")
+		}
 		return false
 	}
 
 	defer conn.Close()
+	println(address, " is open")
 	return true
 }
 
 func resolveHostname(givenHost string) string {
 	addr, err := net.LookupHost(givenHost)
-	fmt.Printf("%v\n", len(addr))
+	fmt.Printf("%v\n", (addr))
 
 	if err != nil || len(addr) < 1 {
 		fmt.Printf("%v\n", addr)
@@ -32,20 +69,14 @@ func resolveHostname(givenHost string) string {
 func main() {
 
 	// Defining CLI Flags
-	protoPTR := flag.String("protocol", "tcp", "Set the protocol you want, TCP or UDP. DEFAULT: TCP")
-	hostPTR := flag.String("hostname", "localhost", "Set the hostname you want to connect to. DEFAULT: localhost")
-	portPtr := flag.Int("port", 80, "Set the port to scan. DEFAULT: 80")
-	timeoutPtr := flag.Duration("timeout", 60, "Set the timout allowed on ports. DEFAULT: 60")
+	protoPTR := flag.String("protocol", "tcp", "Set the protocol you want, TCP or UDP.")
+	hostPTR := flag.String("hostname", "localhost", "Set the hostname you want to connect to.")
+	portPtr := flag.Int("port", 80, "Set the port to scan.")
+	timeoutPTR := flag.Duration("timeout", 50, "Set the timout, lower is better but too low would make any port seem closed.")
 
 	// Parse Flags
 	flag.Parse()
 
-	resolveHostname(*hostPTR)
-
-	open := scanPort(*protoPTR, *hostPTR, *portPtr, *timeoutPtr)
-	open2 := scanPort(*protoPTR, *hostPTR, 40, *timeoutPtr)
-
-	fmt.Printf("Connected? %t\n", open)
-	fmt.Printf("Connected? %t\n", open2)
+	scanPort(*protoPTR, *hostPTR, *portPtr, *timeoutPTR)
 
 }
