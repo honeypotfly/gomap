d7ea8b3219977ffcc0ad41e1026ad08e71a7e489
honeypotfly <honeypotfly@protonmail.com>
2022-03-15 00:21:07 -0400

    add flags for proto and host

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"net"
 	"os/exec"
@@ -14,6 +15,7 @@ import (
 )
 
 type PortScanner struct {
+	proto    string
 	hostname string
 	lock     *semaphore.Weighted
 }
@@ -35,16 +37,16 @@ func Ulimit() int64 {
 	return i
 }
 
-func ScanPort(hostname string, port int, maxTimeout time.Duration) {
+func ScanPort(myProto string, hostname string, port int, maxTimeout time.Duration) {
 	address := fmt.Sprintf("%s:%d", hostname, port)
-	conn, err := net.DialTimeout("tcp", address, maxTimeout)
+	conn, err := net.DialTimeout(myProto, address, maxTimeout)
 
 	if err != nil {
 		//fmt.Println(err)
 		if strings.Contains(err.Error(), "too many open files") {
 			time.Sleep(maxTimeout)
 			// If we open too many files, sleep and restart the scan
-			ScanPort(hostname, port, maxTimeout)
+			ScanPort(myProto, hostname, port, maxTimeout)
 		} else {
 			//fmt.Println(port, "closed")
 		}
@@ -55,7 +57,7 @@ func ScanPort(hostname string, port int, maxTimeout time.Duration) {
 	fmt.Println(address, " is open")
 }
 
-func (ps *PortScanner) Start(f, l int, timeout time.Duration) {
+func (ps *PortScanner) Start(myProto string, f, l int, timeout time.Duration) {
 	wg := sync.WaitGroup{}
 	defer wg.Wait()
 
@@ -65,7 +67,7 @@ func (ps *PortScanner) Start(f, l int, timeout time.Duration) {
 		go func(port int) {
 			defer ps.lock.Release(1)
 			defer wg.Done()
-			ScanPort(ps.hostname, port, timeout)
+			ScanPort(myProto, ps.hostname, port, timeout)
 		}(port)
 	}
 }
@@ -73,19 +75,18 @@ func (ps *PortScanner) Start(f, l int, timeout time.Duration) {
 func main() {
 
 	// Defining CLI Flags
-	//protoPTR := flag.String("protocol", "tcp", "Set the protocol you want, TCP or UDP.")
-	//hostPTR := flag.String("hostname", "localhost", "Set the hostname you want to connect to.")
+	protoPTR := flag.String("protocol", "tcp", "Set the protocol you want, TCP or UDP.")
+	hostPTR := flag.String("hostname", "localhost", "Set the hostname you want to connect to.")
 	//portPtr := flag.Int("port", 80, "Set the port to scan.")
 	//timeoutPTR := flag.Duration("timeout", 50, "Set the timout, lower is better but too low would make any port seem closed.")
 
 	// Parse Flags
-	//flag.Parse()
-
-	//scanPort(*protoPTR, *hostPTR, *portPtr, *timeoutPTR)
+	flag.Parse()
 
 	ps := &PortScanner{
-		hostname: "google.com",
+		proto:    *protoPTR,
+		hostname: *hostPTR,
 		lock:     semaphore.NewWeighted(Ulimit()),
 	}
-	ps.Start(1, 65535, 500*time.Millisecond)
+	ps.Start(ps.proto, 1, 65535, 500*time.Millisecond)
 }
